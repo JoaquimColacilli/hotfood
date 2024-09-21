3996bd670184ef40707d0ee3bb62fff57ed7dcd1
Joaquim <joaquimcolacilli9@gmail.com>
2024-09-21 17:21:05 -0300

    se suben ultimos cambios pagina

--- src/app/hotfood/hotfood.component.ts
+++ src/app/hotfood/hotfood.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -14,17 +14,17 @@ export class HotfoodComponent implements OnInit {
   totalImages = 3;
   isCarousel = false;
 
+  constructor(private el: ElementRef) {}
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  // Método para mostrar la imagen correspondiente en modo carrusel
   showImage(index: number): void {
     this.currentIndex = index;
     const offset = -index * 100;
     const images = document.querySelectorAll<HTMLElement>('.hero-image');
 
-    // Verifica que haya suficientes imágenes antes de aplicar el desplazamiento
     if (images.length !== this.totalImages) {
       console.error(
         `Número de imágenes incorrecto. Se esperaban ${this.totalImages}, pero se encontraron ${images.length}`
@@ -32,16 +32,12 @@ export class HotfoodComponent implements OnInit {
       return;
     }
 
-    // Aplica el desplazamiento a todas las imágenes en modo carrusel
     images.forEach((image) => {
       image.style.transform = `translateX(${offset}%)`;
-      image.style.transition = 'transform 0.5s ease-in-out'; // Transición suave
+      image.style.transition = 'transform 0.5s ease-in-out';
     });
-
-    console.log(`Mostrando imagen ${index} con offset: ${offset}%`);
   }
 
-  // Método para manejar el clic en los botones de navegación
   navigate(direction: 'prev' | 'next'): void {
     if (direction === 'prev') {
       this.currentIndex =
@@ -53,32 +49,49 @@ export class HotfoodComponent implements OnInit {
     this.showImage(this.currentIndex);
   }
 
-  // Método para restablecer la posición de las imágenes cuando se sale del modo carrusel
   resetImagesPosition(): void {
     const images = document.querySelectorAll<HTMLElement>('.hero-image');
-
-    // Restablece la posición de cada imagen al estado inicial
     images.forEach((image) => {
       image.style.transform = `translateX(0%)`;
       image.style.transition = 'transform 0.5s ease-in-out';
     });
   }
 
-  // Listener para detectar el cambio de tamaño de pantalla
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.isCarousel = window.innerWidth <= 880;
-
-    // Si se sale del modo carrusel, restablece las posiciones de las imágenes
     if (!this.isCarousel) {
       this.resetImagesPosition();
     }
   }
+  observeElements() {
+    const elements = this.el.nativeElement.querySelectorAll(
+      '.section-1 .hero, .section-1 .chalkboard-section, .section-1 .burger-grid .burger-item, .section-1 .vegan-section, .sides-grid .burger-item, .section-2 .limited-edition-title, .section-2 .image-container, .section-3 .text-section, .section-3 .polaroid-container, .section-4 .nuestro-local-title, .section-4 .nuestro-local-text, .section-4 .map-iframe, .footer-section .footer-contact, .footer-section .footer-logo, .footer-section .footer-work'
+    );
 
-  // Inicializar la vista de la primera imagen
+    const options = {
+      root: null,
+      threshold: 0.2,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+          observer.unobserve(entry.target); // Deja de observar una vez que se activa la animación
+        }
+      });
+    }, options);
+
+    elements.forEach((element: HTMLElement) => {
+      observer.observe(element);
+    });
+  }
+
   ngOnInit() {
-    this.isCarousel = window.innerWidth <= 880; // Verifica el tamaño inicial de la pantalla
-    this.currentIndex = 0; // Asegura que el índice inicial sea 0
-    this.showImage(this.currentIndex); // Muestra la primera imagen al cargar
+    this.isCarousel = window.innerWidth <= 880;
+    this.currentIndex = 0;
+    this.showImage(this.currentIndex);
+    this.observeElements();
   }
 }
